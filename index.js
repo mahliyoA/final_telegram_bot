cc24a10911ca6dce399d9a59bcadd8375f900b27
Makhliyo <azimovamahliyo>
2025-12-04 15:49:07 +0500

    done

---
+++ index.js
@@ -0,0 +1,16 @@
+import "./src/bot.js"
+import mongoose from "mongoose";
+import { config } from "dotenv";
+config();
+
+
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log(`Db is connected...`);
+    })
+    .catch(() => {
+        console.log(`Error: db is not connected!`);
+    });
+
+console.log("bot ishga tushdi...");
---
+++ src/bot/bot.js
@@ -0,0 +1,15 @@
+import TelegramBot from "node-telegram-bot-api";
+import { config } from "dotenv";
+config()
+
+const bot = TelegramBot(process.env.BOT_TOKEN, { polling: true })
+
+bot.on("message", (msg) => {
+    const chatId = msg.chat.id;
+    const firstname = msg.chat.first_name;
+    const text = msg.text;
+
+    bot.sendMessage(chatId, `assalomu aleykum, ${firstname}`);
+    bot.sendMessage(`chatId, ${text}`)
+})
+console.log("bot ishga tushdi...");
