3609e8db607f1889adcf35299447c31e1abff914
Makhliyo <azimovamahliyo>
2025-12-06 16:01:20 +0500

    done

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-import "./src/bot.js"
+import "./src/bot/bot.js";
 import mongoose from "mongoose";
 import { config } from "dotenv";
 config();
 
 
 mongoose
-    .connect(process.env.MONGO_URI)
-    .then(() => {
-        console.log(`Db is connected...`);
-    })
-    .catch(() => {
-        console.log(`Error: db is not connected!`);
-    });
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log(`Db is connected...`);
+  })
+  .catch(() => {
+    console.log(`Error: db is not connected!`);
+  });
 
-console.log("bot ishga tushdi...");
+console.log("Dastur boshlanmoqda...");
--- src/bot/bot.js
+++ src/bot/bot.js
@@ -1,15 +1,26 @@
 import TelegramBot from "node-telegram-bot-api";
 import { config } from "dotenv";
-config()
+import onStart from "./handlers/onStart.js";
+import onProfile from "./handlers/onProfile.js";
+import onError from "./handlers/onError.js";
+config();
 
-const bot = TelegramBot(process.env.BOT_TOKEN, { polling: true })
+export const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
 
 bot.on("message", (msg) => {
-    const chatId = msg.chat.id;
-    const firstname = msg.chat.first_name;
-    const text = msg.text;
+  const chatId = msg.chat.id;
+  const firstname = msg.chat.first_name;
+  const text = msg.text;
 
-    bot.sendMessage(chatId, `assalomu aleykum, ${firstname}`);
-    bot.sendMessage(`chatId, ${text}`)
-})
-console.log("bot ishga tushdi...");
+  if (text == "/start") {
+    return onStart(msg);
+  }
+
+  if (text == "/profile") {
+    return onProfile(msg);
+  }
+
+  return onError(msg);
+});
+
+console.log("Bot ishga tushdi...");
